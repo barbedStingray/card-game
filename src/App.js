e6f75f154eefbbeae5325185a2c62a3d911f0539
Ben Rehmann <brehmann@me.com>
2024-09-04 13:45:51 -0500

    board setup

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   const [gameCount, setGameCount] = useState(0)
   const [toonOrderArray, setToonOrderArray] = useState(dToons)
 
-  const [boardSlots, setBoardSlots] = useState(Array(8).fill(null))
+  const [boardSlots, setBoardSlots] = useState(Array(14).fill(null))
   const [boardScore, setBoardScore] = useState(0)
   const [myToonScore, setMyToonScore] = useState(0)
   const [opponentScore, setOpponentScore] = useState(0)
@@ -73,74 +73,49 @@ function App() {
   return (
     <div className="App">
 
+
       <div className='gameBoard'>
-        <div className='gameside'>
-          {myToons.map((dtoon, i) => (
-            <div className='dToonCard' key={i}>
-              {dtoon ? (
-                <>
-                <div className='dToonDiv' style={{ color: dtoon.active ? '' : 'red' }}>
-                  <img className='toonImage' src={dtoon.displayImage} />
-                  <div>
-                    <p>{dtoon.character}</p>
-                    <p>{dtoon.color} {dtoon.points} {dtoon.kind}</p>
-                    <p>{dtoon.groups}</p>
-                    <p>{dtoon.gender}</p>
-                    <p>{dtoon.role}</p>
-                    <p>{dtoon.rarity}</p>
-                    <p>{dtoon.movie}</p>
-                  </div>
-                </div>
-                  {dtoon.abilities.map((ability, i) => (
-                    <p key={i}>{ability.ability}</p>
-                  ))}
-                </>
+
+        <div className='opponentLayout'>
+          {opponentToons.map((slot, index) => (
+            <div className="spot" key={index}>
+              {slot ? (
+                <img className='boardImage' src={slot.displayImage} alt={`Card ${index}`} />
               ) : (
-                <p>EMPTY SLOT</p>
+                <p>No current Card</p>
               )}
             </div>
           ))}
         </div>
 
-        <div className='deckside'>
-          {opponentToons.map((dtoon, i) => (
-            <div className='dToonCard' key={i}>
-              {dtoon ? (
-                <>
-                <div className='dToonDiv' style={{ color: dtoon.active ? '' : 'red' }}>
-                  <img className='toonImage' src={dtoon.displayImage} />
-                  <div>
-                    <p>{dtoon.character}</p>
-                    <p>{dtoon.color} {dtoon.points} {dtoon.kind}</p>
-                    <p>{dtoon.groups}</p>
-                    <p>{dtoon.gender}</p>
-                    <p>{dtoon.role}</p>
-                    <p>{dtoon.rarity}</p>
-                    <p>{dtoon.movie}</p>
-                  </div>
-                </div>
-                  {dtoon.abilities.map((ability, i) => (
-                    <p key={i}>{ability.ability}</p>
-                  ))}
-                </>
+        <div className='scoreBoard'>
+          <button onClick={() => placeCardIntoPlay(boardSlots)}>PLACE</button>
+          <button onClick={() => beginScoringRound(boardSlots)}>PLAY</button>
+          <p>Total</p>
+          <h1>{boardScore}</h1>
+          <p>My Score</p>
+          <h1>{myToonScore}</h1>
+          <p>Opponent</p>
+          <h1>{opponentScore}</h1>
+        </div>
+
+        <div className='playerLayout'>
+          {myToons.map((slot, index) => (
+            <div className="spot" key={index}>
+              {slot ? (
+                <img className='boardImage' src={slot.displayImage} alt={`Card ${index}`} />
               ) : (
-                <p>EMPTY SLOT</p>
+                <p>No current Card</p>
               )}
             </div>
           ))}
         </div>
+
       </div>
 
-      <div className='logistics'>
-        <button onClick={() => placeCardIntoPlay(boardSlots)}>PLACE</button>
-        <button onClick={() => beginScoringRound(boardSlots)}>PLAY</button>
-        <p>Total</p>
-        <h1>{boardScore}</h1>
-        <p>My Score</p>
-        <h1>{myToonScore}</h1>
-        <p>Opponent</p>
-        <h1>{opponentScore}</h1>
-      </div>
+
+      <div className='infoAndDeck'></div>
+
 
     </div>
   );
